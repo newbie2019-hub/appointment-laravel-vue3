cbf9ffea5faf72cd0c0644c51c06144fb6fd2bc7
Yvan Sabay <sabayyvan2018@gmail.com>
2022-11-28 02:04:48 +0800

    ambot

--- public/build/assets/Guest.3e0b811c.js
+++
@@ -1,1 +0,0 @@
-import{i as s,e as t,A as r}from"./app.e7bbbd2e.js";const c={class:"min-h-screen flex flex-col sm:justify-center items-center sm:pt-0 bg-gray-100"},l={__name:"Guest",setup(n){return(e,o)=>(s(),t("div",c,[r(e.$slots,"default")]))}};export{l as _};
